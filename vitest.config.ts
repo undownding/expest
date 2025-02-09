2e9e35738eb20067b691d6b9aab7c9f81521e40e
undownding <undownding@gmail.com>
2025-02-09 20:27:58 +0800

    set reporter to text

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default mergeConfig(
       include: ['test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
       coverage: {
         provider: 'v8',
-        reporter: ['text-summary', 'cobertura'],
+        reporter: ['text', 'cobertura'],
       },
       reporters: ['default', 'junit'],
       outputFile: {
